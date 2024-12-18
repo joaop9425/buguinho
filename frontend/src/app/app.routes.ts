c57cbca06295df6071fd3ddb879624e5d60ec7fa
João Paulo Neves <joao.p9425@gmail.com>
2024-12-18 17:17:17 -0300

    post

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,16 +1,21 @@
-import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { PostsComponent } from './features/posts/posts.component';
+import { Routes } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { PostComponent } from "./features/post/post.component";
+import { PostsComponent } from "./features/posts/posts.component";
 
 export const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: AppComponent,
     children: [
       {
-        path: '',
-        component: PostsComponent
-      }
-    ]
+        path: "",
+        component: PostsComponent,
+      },
+      {
+        path: ":id",
+        component: PostComponent,
+      },
+    ],
   },
 ];
