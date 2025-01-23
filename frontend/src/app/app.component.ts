11231cf4b0aa67f693c94665eb34d35f74b4b262
João Paulo Neves <joao.p9425@gmail.com>
2025-01-23 17:38:41 -0300

    matrix bg

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -27,5 +27,45 @@ export class AppComponent {
   titlePage = 'Buguinho';
   constructor(private title: TitleService, private route: ActivatedRoute) {
     this.title.set(this.titlePage);
+    this.startMatrixEffect();
+  }
+
+  startMatrixEffect() {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    document.body.appendChild(canvas);
+    canvas.style.position = 'fixed';
+    canvas.style.top = '0';
+    canvas.style.left = '0';
+    canvas.style.width = '100%';
+    canvas.style.height = '100%';
+    canvas.style.zIndex = '-1';
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    const columns = canvas.width / 20;
+    const drops: number[] = Array.from({ length: columns }, () => 1);
+
+    function draw() {
+      if (ctx) {
+        ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = '#0F0';
+        ctx.font = '20px monospace';
+
+        drops.forEach((y: any, index) => {
+          const text = Math.random() > 0.5 ? '0' : '1';
+          const x = index * 20;
+          ctx.fillText(text, x, y * 20);
+
+          if (y * 20 > canvas.height && Math.random() > 0.975) {
+            drops[index] = 0;
+          }
+          drops[index]++;
+        });
+      }
+    }
+
+    setInterval(draw, 50);
   }
 }
